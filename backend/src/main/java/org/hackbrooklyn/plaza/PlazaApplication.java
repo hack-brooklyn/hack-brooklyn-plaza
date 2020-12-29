445af7a588be2c6951c8ee3b923118169c21f02b
KevinTamCS <kevin@kevintamcs.com>
2020-12-29 13:25:08 -0500

    Initialize backend

---
+++ backend/src/main/java/org/hackbrooklyn/plaza/PlazaApplication.java
@@ -0,0 +1,13 @@
+package org.hackbrooklyn.plaza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlazaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlazaApplication.class, args);
+	}
+
+}
