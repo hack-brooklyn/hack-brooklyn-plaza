194be530ea7cb78ce94a6a8408c80f0011b56fd8
KevinTamCS <kevin@kevintamcs.com>
2021-01-11 14:45:14 -0500

    Move common form styles to separate file

--- frontend/src/views/ApplicationView/styles.ts
+++ frontend/src/views/ApplicationView/styles.ts
@@ -4,14 +4,14 @@ export const StyledFieldset = styled.fieldset`
   margin: 1.5rem auto 0;
 `;
 
-export const StyledLegend = styled.legend`
-  margin: 0 auto 1rem;
+export const StyledTitleLegend = styled.legend`
+  margin: 0.75rem auto 0.25rem;
   font-size: 1.75rem;
   font-weight: bold;
   text-align: center;
 `;
 
-export const FormFields = styled.div`
+export const ApplicationFormFields = styled.div`
   @media screen and (min-width: 992px) {
     display: grid;
     grid-template-columns: 1fr 1fr;
