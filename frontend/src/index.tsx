6341a39bbf83b82f5130cc2664e8252813102a38
KevinTamCS <kevin@kevintamcs.com>
2021-02-16 12:01:17 -0500

    Add polyfills for older browsers

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,3 +1,9 @@
+// Polyfills
+import 'react-app-polyfill/stable';
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/ie9';
+import 'whatwg-fetch';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
