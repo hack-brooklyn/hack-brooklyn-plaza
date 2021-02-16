fa5c734995c3c712a588b88574f5b830923cc8ec
KevinTamCS <kevin@kevintamcs.com>
2021-02-16 02:02:57 -0500

    Redirect to application page from homepage during the application period

--- frontend/src/views/Home/Home.tsx
+++ frontend/src/views/Home/Home.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 const Home = (): JSX.Element => {
+
+  const history = useHistory();
+
+  // Temporary redirect until the platform opens up
+  useEffect(() => {
+    history.push('/apply');
+  }, []);
+
   return (
     <>
-      <h1>Welcome to Hack Brooklyn Plaza.</h1>
-      <p>Hack Brooklyn Plaza is currently under development. Please check back after acceptances begin releasing.</p>
+      {/*<h1>Welcome to Hack Brooklyn Plaza.</h1>*/}
+      {/*<p>Hack Brooklyn Plaza is currently under development. Please check back after acceptances begin releasing.</p>*/}
     </>
   );
 };
