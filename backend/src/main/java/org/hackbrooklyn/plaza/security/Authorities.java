bfb924aecfcadbea13f716ad805f34e3ecc7edb4
KevinTamCS <kevin@kevintamcs.com>
2021-03-08 21:31:08 -0500

    Add authorities and roles for platform features

---
+++ backend/src/main/java/org/hackbrooklyn/plaza/security/Authorities.java
@@ -0,0 +1,29 @@
+package org.hackbrooklyn.plaza.security;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public final class Authorities {
+    public static final String APPLICATIONS_READ_ALL = "APPLICATIONS_READ_ALL";
+    public static final String APPLICATIONS_READ_INDIVIDUAL = "APPLICATIONS_READ_INDIVIDUAL";
+    public static final String APPLICATIONS_UPDATE_DECISION = "APPLICATIONS_UPDATE_DECISION";
+    public static final String APPLICATIONS_DELETE = "APPLICATIONS_DELETE";
+
+    public static final String USERS_CREATE = "USERS_CREATE";
+    public static final String USERS_UPDATE_ROLE = "USERS_UPDATE_ROLE";
+
+    public static final String ANNOUNCEMENTS_CREATE = "ANNOUNCEMENTS_CREATE";
+    public static final String ANNOUNCEMENTS_READ = "ANNOUNCEMENTS_READ";
+    public static final String ANNOUNCEMENTS_UPDATE = "ANNOUNCEMENTS_UPDATE";
+    public static final String ANNOUNCEMENTS_DELETE = "ANNOUNCEMENTS_DELETE";
+
+    public static final String EVENTS_CREATE = "EVENTS_CREATE";
+    public static final String EVENTS_READ_ALL = "EVENTS_READ_ALL";
+    public static final String EVENTS_READ_INDIVIDUAL = "EVENTS_READ_INDIVIDUAL";
+    public static final String EVENTS_UPDATE = "EVENTS_UPDATE";
+    public static final String EVENTS_DELETE = "EVENTS_DELETE";
+
+    public static final String SAVED_EVENTS_ADD = "SAVED_EVENTS_ADD";
+    public static final String SAVED_EVENTS_READ = "SAVED_EVENTS_READ";
+    public static final String SAVED_EVENTS_REMOVE = "SAVED_EVENTS_REMOVE";
+}
---
+++ backend/src/main/java/org/hackbrooklyn/plaza/security/Roles.java
@@ -0,0 +1,104 @@
+package org.hackbrooklyn.plaza.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import java.util.*;
+
+@Component
+public class Roles {
+    public static final String ADMIN = "ROLE_ADMIN";
+    public static final String VOLUNTEER = "ROLE_VOLUNTEER";
+    public static final String PARTICIPANT = "ROLE_PARTICIPANT";
+
+    public static final Set<GrantedAuthority> ADMIN_GRANTED_AUTHORITIES;
+    public static final Set<GrantedAuthority> VOLUNTEER_GRANTED_AUTHORITIES;
+    public static final Set<GrantedAuthority> PARTICIPANT_GRANTED_AUTHORITIES;
+
+    static {
+        // Admins
+        String[] adminAuthorities = {
+                // Applications
+                Authorities.APPLICATIONS_READ_ALL,
+                Authorities.APPLICATIONS_READ_INDIVIDUAL,
+                Authorities.APPLICATIONS_UPDATE_DECISION,
+                Authorities.APPLICATIONS_DELETE,
+
+                // Announcements
+                Authorities.ANNOUNCEMENTS_CREATE,
+                Authorities.ANNOUNCEMENTS_READ,
+                Authorities.ANNOUNCEMENTS_UPDATE,
+                Authorities.ANNOUNCEMENTS_DELETE,
+
+                // Users
+                Authorities.USERS_CREATE,
+                Authorities.USERS_UPDATE_ROLE,
+
+                // Events
+                Authorities.EVENTS_CREATE,
+                Authorities.EVENTS_READ_ALL,
+                Authorities.EVENTS_READ_INDIVIDUAL,
+                Authorities.EVENTS_UPDATE,
+                Authorities.EVENTS_DELETE,
+
+                // Saved Events
+                Authorities.SAVED_EVENTS_ADD,
+                Authorities.SAVED_EVENTS_REMOVE,
+                Authorities.SAVED_EVENTS_READ,
+        };
+
+        // Volunteers
+        String[] volunteerAuthorities = {
+                // Announcements
+                Authorities.ANNOUNCEMENTS_READ,
+
+                // Events
+                Authorities.EVENTS_READ_ALL,
+                Authorities.EVENTS_READ_INDIVIDUAL,
+
+                // Saved Events
+                Authorities.SAVED_EVENTS_ADD,
+                Authorities.SAVED_EVENTS_REMOVE,
+                Authorities.SAVED_EVENTS_READ,
+        };
+
+        // Participants
+        String[] participantAuthorities = {
+                // Announcements
+                Authorities.ANNOUNCEMENTS_READ,
+
+                // Events
+                Authorities.EVENTS_READ_ALL,
+                Authorities.EVENTS_READ_INDIVIDUAL,
+
+                // Saved Events
+                Authorities.SAVED_EVENTS_ADD,
+                Authorities.SAVED_EVENTS_REMOVE,
+                Authorities.SAVED_EVENTS_READ,
+        };
+
+        PARTICIPANT_GRANTED_AUTHORITIES = createGrantedAuthoritySet(participantAuthorities);
+        VOLUNTEER_GRANTED_AUTHORITIES = createGrantedAuthoritySet(volunteerAuthorities);
+        ADMIN_GRANTED_AUTHORITIES = createGrantedAuthoritySet(adminAuthorities);
+    }
+
+    public static Set<GrantedAuthority> getGrantedAuthoritiesForRole(String role) {
+        switch (role) {
+            case "ROLE_ADMIN":
+                return ADMIN_GRANTED_AUTHORITIES;
+            case "ROLE_VOLUNTEER":
+                return VOLUNTEER_GRANTED_AUTHORITIES;
+            case "ROLE_PARTICIPANT":
+                return PARTICIPANT_GRANTED_AUTHORITIES;
+            default:
+                return null;
+        }
+    }
+
+    private static Set<GrantedAuthority> createGrantedAuthoritySet(String[] authorities) {
+        List<GrantedAuthority> grantedAuthorities = new ArrayList<>(authorities.length);
+        Arrays.asList(authorities).forEach(authority -> grantedAuthorities.add(new SimpleGrantedAuthority(authority)));
+        return Collections.unmodifiableSet(new HashSet<>(grantedAuthorities));
+    }
+}
