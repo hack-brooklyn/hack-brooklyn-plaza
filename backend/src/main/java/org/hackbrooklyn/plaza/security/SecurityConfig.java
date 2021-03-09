d4b73c7b6cd669457cfedc6a598aade883c8e404
KevinTamCS <kevin@kevintamcs.com>
2021-03-08 21:33:17 -0500

    Add Spring Security configuration

---
+++ backend/src/main/java/org/hackbrooklyn/plaza/security/SecurityConfig.java
@@ -0,0 +1,92 @@
+package org.hackbrooklyn.plaza.security;
+
+import org.hackbrooklyn.plaza.service.PlazaUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${FRONTEND_DOMAIN}")
+    private String FRONTEND_DOMAIN;
+
+    @Value("${BCRYPT_SALT_ROUNDS}")
+    private int BCRYPT_SALT_ROUNDS;
+
+    private final PlazaUserDetailsService userDetailsService;
+    private final JwtTokenFilter jwtTokenFilter;
+
+    @Autowired
+    public SecurityConfig(PlazaUserDetailsService userDetailsService, JwtTokenFilter jwtTokenFilter) {
+        this.userDetailsService = userDetailsService;
+        this.jwtTokenFilter = jwtTokenFilter;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                // Public endpoints
+                .mvcMatchers(HttpMethod.GET, "/").permitAll()
+                .mvcMatchers(HttpMethod.POST, "/apply", "/apply/checkPriorityEligibility").permitAll()
+                .mvcMatchers(HttpMethod.POST, "/newsletter/subscribe").permitAll()
+                .mvcMatchers(HttpMethod.POST,
+                        "/users/login",
+                        "/users/activate",
+                        "/users/activate/request",
+                        "/users/resetPassword",
+                        "/users/resetPassword/request").permitAll()
+                .anyRequest().authenticated();
+
+        http.exceptionHandling().authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
+        http.cors().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().csrf().disable();
+        http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin(FRONTEND_DOMAIN);
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(BCRYPT_SALT_ROUNDS);
+    }
+}
