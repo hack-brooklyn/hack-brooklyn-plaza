840ae2cbaf1e2f1d51b722be2fb72d695ea3cae9
KevinTamCS <kevin@kevintamcs.com>
2021-03-11 20:38:19 -0500

    Add Redux to main App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,59 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, useHistory } from 'react-router-dom';
 import styled from 'styled-components/macro';
 import Container from 'react-bootstrap/Container';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-import { Navbar } from 'components';
+import { Navbar } from './components';
 import Routes from './Routes';
+import store from './store';
+import { refreshAccessToken } from 'util/auth';
 
 const App = (): JSX.Element => {
   return (
-    <Router>
+    <Provider store={store}>
+      <Router>
+        <AppContent />
+      </Router>
+
+      <ToastContainer />
+    </Provider>
+  );
+};
+
+const AppContent = () => {
+  const [appReady, setAppReady] = useState(false);
+
+  const history = useHistory();
+
+  useEffect(() => {
+    const isUserLoggedIn = localStorage.getItem('isUserLoggedIn');
+    if (isUserLoggedIn !== null && JSON.parse(isUserLoggedIn)) {
+      const authenticateWithToken = async () => {
+        try {
+          await refreshAccessToken(history);
+        } catch (err) {
+          console.error(err);
+        } finally {
+          setAppReady(true);
+        }
+      };
+
+      authenticateWithToken();
+    } else {
+      localStorage.setItem('isUserLoggedIn', JSON.stringify(false));
+      setAppReady(true);
+    }
+  }, []);
+
+  return appReady ? <MainContent /> : <LoadingContent />;
+};
+
+const MainContent = () => {
+  return (
+    <>
       <header>
         <Navbar />
       </header>
@@ -19,7 +62,6 @@ const App = (): JSX.Element => {
         <Container>
           <Routes />
         </Container>
-        <ToastContainer />
       </StyledMain>
     </Router>
   );
