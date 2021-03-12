e76c947241a780569f45a85115290e5de07e259d
KevinTamCS <kevin@kevintamcs.com>
2021-03-11 20:37:17 -0500

    Add Login view and route

---
+++ frontend/src/views/Login/Login.tsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { FastField, Formik, FormikHelpers } from 'formik';
+import styled from 'styled-components/macro';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+import { RequiredFormLabel } from 'components';
+import { StyledSubmitButton } from 'views/ApplicationPage/components/ApplicationForm';
+import { toast } from 'react-toastify';
+import { LoginData, logInUser } from 'util/auth';
+import { useHistory } from 'react-router-dom';
+
+const Login = (): JSX.Element => {
+    const history = useHistory();
+
+    const initialValues: LoginData = {
+      email: '',
+      password: ''
+    };
+
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    const submitLogin = async (loginFormData: LoginData, { setSubmitting }: FormikHelpers<LoginData>): Promise<void> => {
+      setSubmitting(true);
+
+      try {
+        await logInUser(loginFormData);
+        history.push('/dashboard');
+      } catch (err) {
+        toast.error(err.message);
+      }
+
+      setSubmitting(false);
+    };
+
+    return (
+      <>
+        <StyledH1>Log In to Hack Brooklyn Plaza</StyledH1>
+
+        <Formik
+          initialValues={initialValues}
+          onSubmit={submitLogin}
+        >
+          {formik => (
+            <Form onSubmit={formik.handleSubmit}>
+              <Form.Group controlId="loginEmail">
+                <RequiredFormLabel>Email</RequiredFormLabel>
+                <FastField as={Form.Control}
+                           name="email"
+                           type="email"
+                           disabled={formik.isSubmitting}
+                           required />
+              </Form.Group>
+
+              <Form.Group controlId="loginPassword">
+                <RequiredFormLabel>Password</RequiredFormLabel>
+                <FastField as={Form.Control}
+                           name="password"
+                           type="password"
+                           disabled={formik.isSubmitting}
+                           required />
+              </Form.Group>
+
+              <StyledSubmitButton type="submit" size="lg" disabled={formik.isSubmitting}>
+                Log In
+              </StyledSubmitButton>
+            </Form>
+          )}
+        </Formik>
+      </>
+    );
+  }
+;
+
+const StyledH1 = styled.h1`
+  text-align: center;
+  font-size: 3rem;
+  margin-bottom: 2rem;
+`;
+
+const StyledForm = styled(Form)`
+  margin: 0 auto;
+  max-width: 400px;
+`;
+
+const StyledButton = styled(Button)`
+  margin: 0 auto;
+  text-align: center
+`;
+
+export default Login;
