24e6d3592cc06837e511530aa7c17eb92767b3b7
derickfan <fanderick9999@gmail.com>
2021-03-14 17:52:49 -0400

    Add activation view

--- frontend/src/views/index.ts
+++ frontend/src/views/index.ts
@@ -1,5 +1,6 @@
 export { default as Dashboard } from './Dashboard';
 export { default as Login } from './Login';
 export { default as Landing } from './Landing';
+export { default as Activate } from './Activate';
 export { default as ApplicationLanding } from './ApplicationLanding';
 export { default as ApplicationPage } from './ApplicationPage';
