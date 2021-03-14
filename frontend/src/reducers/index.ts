99156a404e4f0fdaebd1f03d25662ba9cf98af7d
KevinTamCS <kevin@kevintamcs.com>
2021-03-14 13:23:13 -0400

    Add Redux reducer for burger menu

--- frontend/src/reducers/index.ts
+++ frontend/src/reducers/index.ts
@@ -3,11 +3,13 @@ import { combineReducers } from 'redux';
 import appReducer from 'reducers/app';
 import authReducer from 'reducers/auth';
 import userReducer from 'reducers/user';
+import { reducer as burgerMenu } from 'redux-burger-menu';
 
 const reducers = combineReducers({
   app: appReducer,
   auth: authReducer,
-  user: userReducer
+  user: userReducer,
+  burgerMenu
 });
 
 export default reducers;
