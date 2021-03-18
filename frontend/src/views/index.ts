016539699b7aa2a77c40bc5010f35929ad0783ff
KevinTamCS <kevin@kevintamcs.com>
2021-03-18 02:33:15 -0400

    Fix ForgotPassword view imports

--- frontend/src/views/index.ts
+++ frontend/src/views/index.ts
@@ -1,4 +1,5 @@
 export { default as Dashboard } from './Dashboard';
+export { default as ForgotPassword } from './ForgotPassword';
 export { default as Login } from './Login';
 export { default as Landing } from './Landing';
 export { default as Activate } from './Activate';
