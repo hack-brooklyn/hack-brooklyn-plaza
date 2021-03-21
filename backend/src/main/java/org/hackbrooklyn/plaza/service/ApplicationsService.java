0fba79d48f95f4c769b00b9d4ad23425a362765d
KevinTamCS <kevin@kevintamcs.com>
2021-03-21 09:47:29 -0400

    Add service for generating resume download links from AWS S3

--- backend/src/main/java/org/hackbrooklyn/plaza/service/ApplicationsService.java
+++ backend/src/main/java/org/hackbrooklyn/plaza/service/ApplicationsService.java
@@ -1,5 +1,6 @@
 package org.hackbrooklyn.plaza.service;
 
+import org.hackbrooklyn.plaza.dto.LinkDTO;
 import org.hackbrooklyn.plaza.dto.MultipleApplicationsResponse;
 import org.hackbrooklyn.plaza.model.SubmittedApplication;
 
@@ -12,4 +13,6 @@ public interface ApplicationsService {
     void setApplicationDecision(int applicationNumber, SubmittedApplication.Decision decision);
 
     void deleteApplication(int applicationNumber);
+
+    LinkDTO getResumeLink(int applicationNumber);
 }
