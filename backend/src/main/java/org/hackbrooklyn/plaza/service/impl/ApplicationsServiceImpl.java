577a465e9732650c6ec43cd40ba6e54c4f404a30
KevinTamCS <kevin@kevintamcs.com>
2021-03-21 09:47:29 -0400

    Add service for generating resume download links from AWS S3

--- backend/src/main/java/org/hackbrooklyn/plaza/service/impl/ApplicationsServiceImpl.java
+++ backend/src/main/java/org/hackbrooklyn/plaza/service/impl/ApplicationsServiceImpl.java
@@ -2,21 +2,29 @@ package org.hackbrooklyn.plaza.service.impl;
 
 import com.google.common.primitives.Ints;
 import org.hackbrooklyn.plaza.dto.ApplicationManagerEntryDTO;
+import org.hackbrooklyn.plaza.dto.LinkDTO;
 import org.hackbrooklyn.plaza.dto.ApplicationNumbersDTO;
 import org.hackbrooklyn.plaza.dto.MultipleApplicationsResponse;
 import org.hackbrooklyn.plaza.exception.ApplicationNotFoundException;
+import org.hackbrooklyn.plaza.exception.ResumeNotFoundException;
 import org.hackbrooklyn.plaza.model.SubmittedApplication;
 import org.hackbrooklyn.plaza.model.SubmittedApplication_;
 import org.hackbrooklyn.plaza.repository.SubmittedApplicationRepository;
 import org.hackbrooklyn.plaza.service.ApplicationsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedGetObjectRequest;
 
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -28,13 +36,21 @@ import static org.hackbrooklyn.plaza.repository.SubmittedApplicationRepository.A
 @Service
 public class ApplicationsServiceImpl implements ApplicationsService {
 
+    @Value("${AWS_S3_BUCKET}")
+    private String AWS_S3_BUCKET;
+
+    @Value("${AWS_SIGNED_URL_DURATION_MS}")
+    private long AWS_SIGNED_URL_DURATION_MS;
+
     private final SubmittedApplicationRepository submittedApplicationRepository;
     private final EntityManager entityManager;
+    private final S3Presigner s3Presigner;
 
     @Autowired
-    public ApplicationsServiceImpl(SubmittedApplicationRepository submittedApplicationRepository, EntityManager entityManager) {
+    public ApplicationsServiceImpl(SubmittedApplicationRepository submittedApplicationRepository, EntityManager entityManager, S3Presigner s3Presigner) {
         this.submittedApplicationRepository = submittedApplicationRepository;
         this.entityManager = entityManager;
+        this.s3Presigner = s3Presigner;
     }
 
     @Override
@@ -137,5 +153,34 @@ public class ApplicationsServiceImpl implements ApplicationsService {
                 .collect(Collectors.toList());
 
         return new ApplicationNumbersDTO(Ints.toArray(applicationNumbers));
+    }
+
+    @Override
+    public LinkDTO getResumeLink(int applicationNumber) {
+        SubmittedApplication application = submittedApplicationRepository
+                .findFirstByApplicationNumber(applicationNumber)
+                .orElseThrow(ApplicationNotFoundException::new);
+
+        if (application.getResumeKeyS3() == null) {
+            throw new ResumeNotFoundException();
+        }
+
+        // Resume exists, generate a presigned URL to the uploaded resume on AWS S3
+        GetObjectRequest getObjectRequest =
+                GetObjectRequest.builder()
+                        .bucket(AWS_S3_BUCKET)
+                        .key(application.getResumeKeyS3())
+                        .build();
+
+        GetObjectPresignRequest getObjectPresignRequest =
+                GetObjectPresignRequest.builder()
+                        .signatureDuration(Duration.ofMillis(AWS_SIGNED_URL_DURATION_MS))
+                        .getObjectRequest(getObjectRequest)
+                        .build();
+
+        PresignedGetObjectRequest presignedGetObjectRequest =
+                s3Presigner.presignGetObject(getObjectPresignRequest);
+
+        return new LinkDTO(presignedGetObjectRequest.url().toString());
     }
 }
