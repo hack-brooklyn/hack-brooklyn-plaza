771072ebed45c54196910c09fa000f8c237f883f
KevinTamCS <kevin@kevintamcs.com>
2021-03-22 02:05:36 -0400

    Optimize backend imports

--- backend/src/main/java/org/hackbrooklyn/plaza/repository/SubmittedApplicationRepository.java
+++ backend/src/main/java/org/hackbrooklyn/plaza/repository/SubmittedApplicationRepository.java
@@ -9,7 +9,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
