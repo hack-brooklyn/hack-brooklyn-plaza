c61167a94a49db9771e6cadc6b3db795c5891f71
KevinTamCS <kevin@kevintamcs.com>
2021-03-22 22:28:11 -0400

    Add controller endpoint for exporting applications

--- backend/src/main/java/org/hackbrooklyn/plaza/controller/ApplicationsController.java
+++ backend/src/main/java/org/hackbrooklyn/plaza/controller/ApplicationsController.java
@@ -1,5 +1,7 @@
 package org.hackbrooklyn.plaza.controller;
 
+import com.opencsv.exceptions.CsvDataTypeMismatchException;
+import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.hackbrooklyn.plaza.dto.ApplicationNumbersDTO;
@@ -9,6 +11,7 @@ import org.hackbrooklyn.plaza.model.SubmittedApplication;
 import org.hackbrooklyn.plaza.model.SubmittedApplication.Decision;
 import org.hackbrooklyn.plaza.service.ApplicationsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -18,6 +21,9 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import java.io.IOException;
+
+import static org.hackbrooklyn.plaza.model.SubmittedApplication.ExportFormat;
 
 
 @Slf4j
@@ -39,7 +45,7 @@ public class ApplicationsController {
             @RequestParam(defaultValue = "1") @Min(1) int page,
             @RequestParam(defaultValue = "25") @Min(1) int limit,
             @RequestParam(required = false) String searchQuery,
-            @RequestParam(required = false) SubmittedApplication.Decision decision) {
+            @RequestParam(required = false) Decision decision) {
         MultipleApplicationsResponse foundApplications = applicationsService.getMultipleApplications(page, limit, searchQuery, decision);
 
         return new ResponseEntity<>(foundApplications, HttpStatus.OK);
@@ -86,6 +92,33 @@ public class ApplicationsController {
         LinkDTO resumeLinkS3 = applicationsService.getResumeLink(applicationNumber);
 
         return new ResponseEntity<>(resumeLinkS3, HttpStatus.OK);
+    }
+
+    @PreAuthorize("hasAuthority(@authorities.APPLICATIONS_READ)")
+    @GetMapping("/export")
+    public ResponseEntity<byte[]> exportApplications(@RequestParam @Valid ExportFormat type)
+            throws CsvRequiredFieldEmptyException, IOException, CsvDataTypeMismatchException {
+        byte[] exportedFile = applicationsService.exportApplications(type);
+
+        String contentType;
+        switch (type) {
+            case CSV:
+                contentType = "text/csv";
+                break;
+            case JSON:
+                contentType = "application/json";
+                break;
+            case XML:
+                contentType = "application/xml";
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + type);
+        }
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", contentType);
+
+        return new ResponseEntity<>(exportedFile, headers, HttpStatus.OK);
     }
 
     @Data
