a9b8731fe604f1d1fa6fe1cb4a1e78841683c432
KevinTamCS <kevin@kevintamcs.com>
2021-03-24 19:07:58 -0400

    Add DTO for user creation requests

---
+++ backend/src/main/java/org/hackbrooklyn/plaza/dto/CreateUserRequestDTO.java
@@ -0,0 +1,30 @@
+package org.hackbrooklyn.plaza.dto;
+
+import lombok.Data;
+import org.hackbrooklyn.plaza.security.Roles;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Data
+public class CreateUserRequestDTO {
+
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @Size(min = 12)
+    @NotBlank
+    private String password;
+
+    @NotNull
+    private String role;
+}
