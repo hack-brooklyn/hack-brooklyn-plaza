740ca3a76ab23e6ce1f6944a76b55cdbc273739b
derickfan <fanderick9999@gmail.com>
2021-03-24 01:59:13 -0400

    Create AnnouncementView

---
+++ frontend/src/views/AnnouncementView/AnnouncementView.tsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+
+import { AnnouncementBrowser, HeadingButton } from '../../components';
+import { HeadingSection, StyledH1 } from 'commonStyles';
+import { RootState } from '../../types';
+import ac, { Resources } from '../../security/accessControl';
+import penWhiteIcon from 'assets/icons/penWhite.svg';
+
+const AnnouncementView = (): JSX.Element => {
+  const userRole = useSelector((state: RootState) => state.user.role);
+  const [isAbleToModify, setIsAbleToModify] = useState(false);
+
+  useEffect(() => {
+    setIsAbleToModify(checkIfAbleToModify());
+  }, [userRole]);
+
+  const checkIfAbleToModify = (): boolean => {
+    if (userRole !== null) {
+      const permissions = ac.can(userRole).createAny(Resources.Announcements)
+        .attributes;
+      return permissions.includes('*');
+    }
+    return false;
+  };
+
+  return (
+    <>
+      <HeadingSection>
+        <StyledH1>Announcements</StyledH1>
+        {isAbleToModify && (
+          <HeadingButton
+            type={'link'}
+            icon={penWhiteIcon}
+            text={'Post New'}
+            link={'/announcements/post'}
+            onClick={() => {
+              return null;
+            }}
+          />
+        )}
+      </HeadingSection>
+      <AnnouncementBrowser isAbleToModify={isAbleToModify} />
+    </>
+  );
+};
+
+export default AnnouncementView;
