f4dbd2f1223f03ffa18042a042564eef78b4a165
KevinTamCS <kevin@kevintamcs.com>
2021-03-28 19:49:12 -0400

    Add default suggested topics and skills

---
+++ frontend/src/common/defaultTopicsAndSkills.ts
@@ -0,0 +1,32 @@
+import { Option } from 'types';
+import { generateSelectOptions } from 'util/plazaUtils';
+
+export const defaultTopicsAndSkills: Option[] = generateSelectOptions([
+  'Front-End Development',
+  'Back-End Development',
+  'Full-Stack Development',
+  'Mobile Development',
+  'Game Development',
+  'Data Science',
+  'UX/UI Design',
+  'Graphic Design',
+  '3D Modeling',
+  'AR/VR',
+  'AI/Machine Learning',
+  'Blockchain',
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'TypeScript',
+  'Python',
+  'Java',
+  'C#',
+  'C++',
+  'C',
+  'Go',
+  'R',
+  'Swift',
+  'PHP',
+  'Rust',
+  'Ruby'
+]);
