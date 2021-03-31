778ac0556f49ecda6da23fbc52ea9818cddd529a
KevinTamCS <kevin@kevintamcs.com>
2021-03-30 21:02:33 -0400

    Add numbers to allowed topic or skill name characters

--- backend/src/main/java/org/hackbrooklyn/plaza/util/TeamFormationUtils.java
+++ backend/src/main/java/org/hackbrooklyn/plaza/util/TeamFormationUtils.java
@@ -5,7 +5,7 @@ public class TeamFormationUtils {
     // Replace everything that isn't a letter or a hyphen with a hyphen and convert to lowercase.
     public static String cleanTopicOrSkillName(String originalName) {
         return originalName
-                .replaceAll("[^a-zA-Z-]", "-")
+                .replaceAll("[^a-zA-Z0-9-]", "-")
                 .toLowerCase();
     }
 }
