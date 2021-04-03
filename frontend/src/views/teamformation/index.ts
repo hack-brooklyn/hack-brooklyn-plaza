e0d040efd7170066fae9db84669bb81d9b45143c
KevinTamCS <kevin@kevintamcs.com>
2021-04-03 16:31:05 -0400

    Add team formation search page

--- frontend/src/views/teamformation/index.ts
+++ frontend/src/views/teamformation/index.ts
@@ -2,5 +2,6 @@ export { default as TeamFormationHome } from './TeamFormationHome';
 export { default as TeamFormationParticipantSetup } from './TeamFormationParticipantSetup';
 export { default as TeamFormationTeamSetup } from './TeamFormationTeamSetup';
 export { default as TeamFormationParticipantHome } from './TeamFormationParticipantHome';
+export { default as TeamFormationParticipantSearch } from './TeamFormationParticipantSearch';
 export { default as TeamFormationTeamHome } from './TeamFormationTeamHome';
 export { default as TeamFormationTeamSearch } from './TeamFormationTeamSearch';
