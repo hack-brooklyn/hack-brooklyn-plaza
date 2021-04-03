1974d7f82a67a4cddb177f877745adc5efa37aec
KevinTamCS <kevin@kevintamcs.com>
2021-04-03 01:58:55 -0400

    Add team search view

--- frontend/src/views/index.ts
+++ frontend/src/views/index.ts
@@ -17,3 +17,4 @@ export { default as TeamFormationHome } from './TeamFormationHome';
 export { default as TeamFormationParticipantSetup } from './TeamFormationParticipantSetup';
 export { default as TeamFormationTeamSetup } from './TeamFormationTeamSetup';
 export { default as TeamFormationTeamHome } from './TeamFormationTeamHome';
+export { default as TeamFormationTeamSearch } from './TeamFormationTeamSearch';
