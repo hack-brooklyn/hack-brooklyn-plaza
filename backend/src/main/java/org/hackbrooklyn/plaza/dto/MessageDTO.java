6c3f06d441e880e0412d898c0f787958579b6fe3
KevinTamCS <kevin@kevintamcs.com>
2021-04-04 00:45:16 -0400

    Add message DTO

---
+++ backend/src/main/java/org/hackbrooklyn/plaza/dto/MessageDTO.java
@@ -0,0 +1,13 @@
+package org.hackbrooklyn.plaza.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class MessageDTO {
+
+    private String message;
+}
