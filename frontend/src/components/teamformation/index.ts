e86dc1f4cc730e29d4854fcacc2aeebc1c4c79c8
KevinTamCS <kevin@kevintamcs.com>
2021-04-04 16:49:21 -0400

    Add modal for team join requests

--- frontend/src/components/teamformation/index.ts
+++ frontend/src/components/teamformation/index.ts
@@ -2,3 +2,4 @@ export { default as TeamCard } from './TeamCard';
 export { default as ParticipantCard } from './ParticipantCard';
 export { default as ParticipantHeadingSection } from './ParticipantHeadingSection';
 export { default as TopicsAndSkillsSelect } from './TopicsAndSkillsSelect';
+export { default as TeamJoinRequestModal } from './TeamJoinRequestModal';
