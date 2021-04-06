124e81a17e07e665f0bf46ce500fc7e669cf85df
KevinTamCS <kevin@kevintamcs.com>
2021-04-06 17:42:28 -0400

    Fix references for application DTOs

--- backend/src/main/java/org/hackbrooklyn/plaza/service/ApplicationsService.java
+++ backend/src/main/java/org/hackbrooklyn/plaza/service/ApplicationsService.java
@@ -4,7 +4,7 @@ import com.opencsv.exceptions.CsvDataTypeMismatchException;
 import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
 import org.hackbrooklyn.plaza.dto.ApplicationNumbersDTO;
 import org.hackbrooklyn.plaza.dto.LinkDTO;
-import org.hackbrooklyn.plaza.dto.MultipleApplicationsResponse;
+import org.hackbrooklyn.plaza.dto.MultipleApplicationsDTO;
 import org.hackbrooklyn.plaza.model.SubmittedApplication;
 
 import java.io.IOException;
@@ -14,7 +14,7 @@ import static org.hackbrooklyn.plaza.model.SubmittedApplication.ExportFormat;
 
 public interface ApplicationsService {
 
-    MultipleApplicationsResponse getMultipleApplications(int page, int limit, String searchQuery, Decision decision);
+    MultipleApplicationsDTO getMultipleApplications(int page, int limit, String searchQuery, Decision decision);
 
     SubmittedApplication getIndividualApplication(int applicationNumber);
 
