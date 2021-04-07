55ea545790a885abc9cad19252a548491fc5fe1c
KevinTamCS <kevin@kevintamcs.com>
2021-04-07 12:54:10 -0400

    Add repository methods for getting necessary invitation data

--- backend/src/main/java/org/hackbrooklyn/plaza/repository/TeamFormationParticipantInvitationRepository.java
+++ backend/src/main/java/org/hackbrooklyn/plaza/repository/TeamFormationParticipantInvitationRepository.java
@@ -1,10 +1,24 @@
 package org.hackbrooklyn.plaza.repository;
 
+import org.hackbrooklyn.plaza.model.TeamFormationParticipant;
 import org.hackbrooklyn.plaza.model.TeamFormationParticipantInvitation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface TeamFormationParticipantInvitationRepository extends JpaRepository<TeamFormationParticipantInvitation, Integer> {
 
+    List<InvitationIdsOnly> findAllByInvitedParticipantAndInvitationAcceptedNullOrderByInvitationTimestamp(TeamFormationParticipant invitedParticipant);
+
+    @Query("select inv from TeamFormationParticipantInvitation inv join fetch inv.invitingTeam where inv.invitationId = ?1")
+    Optional<TeamFormationParticipantInvitation> findByIdLoadTeam(int id);
+
+    interface InvitationIdsOnly {
+
+        int getInvitationId();
+    }
 }
