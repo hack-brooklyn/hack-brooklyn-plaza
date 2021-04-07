6655a48aec17a6f9c874118e6a1aa7f53388453b
KevinTamCS <kevin@kevintamcs.com>
2021-04-07 12:35:42 -0400

    Adjust Lombok annotations for team formation participant DTO

--- backend/src/main/java/org/hackbrooklyn/plaza/dto/CreateTFParticipantDTO.java
+++ backend/src/main/java/org/hackbrooklyn/plaza/dto/CreateTFParticipantDTO.java
@@ -1,15 +1,19 @@
 package org.hackbrooklyn.plaza.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.Set;
 
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
+@RequiredArgsConstructor
 public class CreateTFParticipantDTO {
 
     @NotNull
