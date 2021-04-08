103e008273db60d77b4bd4a963c1ac0b6a10da77
KevinTamCS <kevin@kevintamcs.com>
2021-04-08 12:13:32 -0400

    Add team data retrieval to team formation utilities

--- frontend/src/util/teamFormation.ts
+++ frontend/src/util/teamFormation.ts
@@ -5,10 +5,11 @@ import { refreshAccessToken } from 'util/auth';
 import { API_ROOT } from 'index';
 import {
   ConnectionError,
-  InvalidSubmittedDataError,
   NoPermissionError,
   TeamFormationParticipant,
+  TeamFormationParticipantNotInTeamError,
   TeamFormationParticipantNotSetUpError,
+  TeamFormationTeam,
   UnknownError
 } from 'types';
 
@@ -22,7 +23,7 @@ export const getParticipantData = async (
 
   let res;
   try {
-    res = await fetch(`${API_ROOT}/teamFormation/participants/userData`, {
+    res = await fetch(`${API_ROOT}/teamFormation/participants/currentUser`, {
       method: 'GET',
       headers: {
         Authorization: `Bearer ${token}`
@@ -37,8 +38,6 @@ export const getParticipantData = async (
   } else if (res.status === 404) {
     history.push('/teamformation');
     throw new TeamFormationParticipantNotSetUpError();
-  } else if (res.status === 400) {
-    throw new InvalidSubmittedDataError();
   } else if (res.status === 401) {
     const refreshedToken = await refreshAccessToken(history);
     await getParticipantData(history, refreshedToken);
@@ -48,6 +47,43 @@ export const getParticipantData = async (
   }
 
   // The function must return a TeamFormationParticipant, otherwise throw an error
+  throw new UnknownError();
+};
+
+export const getTeamData = async (
+  history: History<LocationState>,
+  overriddenAccessToken?: string
+): Promise<TeamFormationTeam> => {
+  const state = store.getState();
+  const accessToken = state.auth.jwtAccessToken;
+  const token = overriddenAccessToken ? overriddenAccessToken : accessToken;
+
+  let res;
+  try {
+    res = await fetch(`${API_ROOT}/teamFormation/teams/currentUser`, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+  } catch (err) {
+    throw new ConnectionError();
+  }
+
+  if (res.status === 200) {
+    return await res.json();
+  } else if (res.status === 404) {
+    history.push('/teamformation');
+    throw new TeamFormationParticipantNotInTeamError();
+  } else if (res.status === 401) {
+    const refreshedToken = await refreshAccessToken(history);
+    await getTeamData(history, refreshedToken);
+  } else if (res.status === 403) {
+    history.push('/');
+    throw new NoPermissionError();
+  }
+
+  // The function must return a TeamFormationTeam, otherwise throw an error
   throw new UnknownError();
 };
 
