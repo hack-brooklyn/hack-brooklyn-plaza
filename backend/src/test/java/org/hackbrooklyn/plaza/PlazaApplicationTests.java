b2abcb9009a8b55ace2e049bf6aa83af4fde320d
KevinTamCS <kevin@kevintamcs.com>
2021-04-13 21:26:26 -0400

    Add startup items to fix tests

--- backend/src/test/java/org/hackbrooklyn/plaza/PlazaApplicationTests.java
+++ backend/src/test/java/org/hackbrooklyn/plaza/PlazaApplicationTests.java
@@ -1,12 +1,28 @@
 package org.hackbrooklyn.plaza;
 
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
+import java.security.Security;
+import java.util.TimeZone;
+
 @SpringBootTest
 @TestPropertySource(locations = "classpath:application-test.properties")
 class PlazaApplicationTests {
+
+    @BeforeAll
+    static void initializeTestEnvironment() {
+        // Set app-wide timezone to UTC for consistency across environments
+        TimeZone.setDefault(TimeZone.getTimeZone("Etc/UTC"));
+
+        // Add BouncyCastle to Security
+        if (Security.getProvider(BouncyCastleProvider.PROVIDER_NAME) == null) {
+            Security.addProvider(new BouncyCastleProvider());
+        }
+    }
 
     @Test
     void contextLoads() {
--- backend/src/test/java/org/hackbrooklyn/plaza/controller/ApplyTests.java
+++ backend/src/test/java/org/hackbrooklyn/plaza/controller/ApplyTests.java
@@ -1,7 +1,9 @@
 package org.hackbrooklyn.plaza.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.hackbrooklyn.plaza.model.SubmittedApplication;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +13,9 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.security.Security;
+import java.util.TimeZone;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -24,6 +29,17 @@ public class ApplyTests {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @BeforeClass
+    public static void initializeTestEnvironment() {
+        // Set app-wide timezone to UTC for consistency across environments
+        TimeZone.setDefault(TimeZone.getTimeZone("Etc/UTC"));
+
+        // Add BouncyCastle to Security
+        if (Security.getProvider(BouncyCastleProvider.PROVIDER_NAME) == null) {
+            Security.addProvider(new BouncyCastleProvider());
+        }
+    }
 
     @Test
     public void shouldSubmitApplicationWithRequiredFields() throws Exception {
