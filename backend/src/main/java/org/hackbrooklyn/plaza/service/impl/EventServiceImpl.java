bf67815749c5619074fa82ed5dbb7073b1ea3b8c
KevinTamCS <kevin@kevintamcs.com>
2021-04-20 00:25:42 -0400

    Schedule event reminder push notifications on event creation and update

--- backend/src/main/java/org/hackbrooklyn/plaza/service/impl/EventServiceImpl.java
+++ backend/src/main/java/org/hackbrooklyn/plaza/service/impl/EventServiceImpl.java
@@ -1,23 +1,31 @@
 package org.hackbrooklyn.plaza.service.impl;
 
+import lombok.extern.slf4j.Slf4j;
 import org.hackbrooklyn.plaza.dto.SaveEventDTO;
 import org.hackbrooklyn.plaza.exception.EventNotFoundException;
 import org.hackbrooklyn.plaza.model.Event;
 import org.hackbrooklyn.plaza.repository.EventRepository;
 import org.hackbrooklyn.plaza.service.EventService;
+import org.hackbrooklyn.plaza.util.PushNotificationUtils;
+import org.hackbrooklyn.plaza.util.SendEventPushNotificationsTask;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.ZoneId;
 import java.util.Collection;
+import java.util.Timer;
 
+@Slf4j
 @Service
 public class EventServiceImpl implements EventService {
 
     private final EventRepository eventRepository;
+    private final PushNotificationUtils pushNotificationUtils;
 
     @Autowired
-    public EventServiceImpl(EventRepository eventRepository) {
+    public EventServiceImpl(EventRepository eventRepository, PushNotificationUtils pushNotificationUtils) {
         this.eventRepository = eventRepository;
+        this.pushNotificationUtils = pushNotificationUtils;
     }
 
     @Override
@@ -39,8 +47,14 @@ public class EventServiceImpl implements EventService {
         event.setStartTime(reqBody.getStartTime());
         event.setEndTime(reqBody.getEndTime());
         event.setPresenters(reqBody.getPresenters());
-
         Event newEvent = eventRepository.save(event);
+
+        try {
+            scheduleEventPushNotification(event);
+        } catch (Exception e) {
+            log.warn(String.format("Unable to schedule push notification for new event with ID: %s", newEvent.getId()));
+            e.printStackTrace();
+        }
 
         return newEvent.getId();
     }
@@ -54,8 +68,14 @@ public class EventServiceImpl implements EventService {
         event.setStartTime(reqBody.getStartTime());
         event.setEndTime(reqBody.getEndTime());
         event.setPresenters(reqBody.getPresenters());
-
         eventRepository.save(event);
+
+        try {
+            scheduleEventPushNotification(event);
+        } catch (Exception e) {
+            log.warn(String.format("Unable to schedule push notification for edited event with ID: %s", event.getId()));
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -64,5 +84,16 @@ public class EventServiceImpl implements EventService {
         eventRepository.delete(event);
     }
 
+    private void scheduleEventPushNotification(Event event) {
+        long currentTimeMs = System.currentTimeMillis();
+        long eventStartTimeMs = event.getStartTime().atZone(ZoneId.of("UTC")).toInstant().toEpochMilli();
 
+        if (eventStartTimeMs > currentTimeMs) {
+            log.warn(String.format("Scheduling a push notification for event ID: %s", event.getId()));
+            new Timer().schedule(
+                    new SendEventPushNotificationsTask(event, pushNotificationUtils, eventRepository),
+                    eventStartTimeMs - currentTimeMs
+            );
+        }
+    }
 }
