8e7f6d47b45da8b8f015150a47f19ac0540da599
derickfan <fanderick9999@gmail.com>
2021-04-21 20:03:42 -0400

    Fix merge conflict

--- frontend/src/components/announcements/Announcement.tsx
+++ frontend/src/components/announcements/Announcement.tsx
@@ -111,12 +111,12 @@ const Announcement = (props: AnnouncementProps): JSX.Element => {
         {displayControls && (
           <ControlContainer>
             <StyledAnchor to={`/announcements/${id}/edit`}>
-              <ButtonIcon src={editIcon} alt="Edit Icon" />
+              <ButtonIcon src={editIcon} alt="Edit Announcement" />
             </StyledAnchor>
             <StyledAnchor to="/announcements">
               <ButtonIcon
                 src={deleteIcon}
-                alt="Delete Icon"
+                alt="Delete Announcement"
                 onClick={confirmDeleteAnnouncement}
               />
             </StyledAnchor>
@@ -126,6 +126,7 @@ const Announcement = (props: AnnouncementProps): JSX.Element => {
     </AnnouncementContainer>
   );
 };
+
 
 const LinkRenderer = (props: LinkProps) => {
   const { href, children } = props;
