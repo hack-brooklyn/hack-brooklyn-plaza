582c78cb20fe2c7797c1a7465d7e3bbc03ffa720
KevinTamCS <kevin@kevintamcs.com>
2021-04-22 12:35:33 -0400

    Fix import

--- frontend/src/components/announcements/AnnouncementEditor.tsx
+++ frontend/src/components/announcements/AnnouncementEditor.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Form } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form';
 import styled from 'styled-components/macro';
 
 import { RequiredFormLabel } from '../';
